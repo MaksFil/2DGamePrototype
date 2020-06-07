b88e453dd13c2e3d5a2a659b6336ba8deb6b10b0
MaksFil <62222429+MaksFil@users.noreply.github.com>
2020-06-07 03:52:32 +0300

    Add files via upload

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    [SerializeField] private float _speed = 0.1f;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private Transform groundCheck;
     private Animator _animator;
+    private Rigidbody2D _physics;
+    private float checkRadius = 0.5f;
+    [SerializeField] private float _speed = 0.1f, jumpForce = 3;
     private int _direction;
-    [SerializeField] private float heightJump = 3;
+    private bool isGrounded;
 
     private void Start() 
     {
         _animator = GetComponent<Animator>();
+        _physics = GetComponent<Rigidbody2D>();
     }
 
     private void Walking(int rotate) 
@@ -25,6 +30,8 @@ public class PlayerController : MonoBehaviour {
         if (_direction > 0) Walking(0);
         else if (_direction < 0) Walking(180);
         else _animator.SetFloat("walking", 0);
+
+        
     }
 
     public void OnClickMoveButton(int direction) 
@@ -35,6 +42,10 @@ public class PlayerController : MonoBehaviour {
 
     public void OnClickJumpButton() 
     {
-        
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        if(isGrounded) 
+        {
+            _physics.velocity = Vector2.up * jumpForce;
+        }
     }
 }
