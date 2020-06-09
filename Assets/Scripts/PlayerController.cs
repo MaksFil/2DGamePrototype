a493b4a3dd6d7c85e7f764fc638d966449cc8f7d
MaksFil <62222429+MaksFil@users.noreply.github.com>
2020-06-09 21:19:58 +0300

    Add files via upload

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,46 +6,45 @@ public class PlayerController : MonoBehaviour {
 
     [SerializeField] private LayerMask whatIsGround;
     [SerializeField] private Transform groundCheck;
+    [SerializeField] private float _speed = 0.1f, jumpForce = 3;
     private Animator _animator;
     private Rigidbody2D _physics;
+    private Collider2D _hammer;
     private float checkRadius = 0.5f;
-    [SerializeField] private float _speed = 0.1f, jumpForce = 3;
     private int _direction;
     private bool isGrounded;
+    [SerializeField] private bool withHammer;
+    public void OnClickMoveButton(int direction)
+    {
+        _animator.SetFloat("walking", direction);
+        _direction = direction;
+    }
 
+    public void OnClickJumpButton()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        if (isGrounded)
+        {
+            _physics.velocity = Vector2.up * jumpForce;
+        }
+    }
+    private void Walking(int rotate)
+    {
+        transform.rotation = Quaternion.Euler(0, rotate, 0);
+        transform.Translate(_speed, 0, 0);
+    }
     private void Start() 
     {
         _animator = GetComponent<Animator>();
         _physics = GetComponent<Rigidbody2D>();
     }
 
-    private void Walking(int rotate) 
-    {
-        transform.rotation = Quaternion.Euler(0, rotate, 0);
-        transform.Translate(_speed, 0, 0);
-    }
-
-    private void Update()
+    private void FixedUpdate()
     {
         if (_direction > 0) Walking(0);
         else if (_direction < 0) Walking(180);
         else _animator.SetFloat("walking", 0);
-
-        
     }
 
-    public void OnClickMoveButton(int direction) 
-    {
-        _animator.SetFloat("walking", direction);
-        _direction = direction;
-    }
-
-    public void OnClickJumpButton() 
-    {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-        if(isGrounded) 
-        {
-            _physics.velocity = Vector2.up * jumpForce;
-        }
-    }
+    
 }
