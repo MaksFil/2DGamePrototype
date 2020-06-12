d698331ef662e47a5af136bd7aaf32eafb87c14f
MaksFil <62222429+MaksFil@users.noreply.github.com>
2020-06-12 14:00:57 +0300

    Add files via upload

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+	[SerializeField] private Slider slider;
+
+	private static float health;
+	public static void AdjustHealthValue(float adjust)
+	{
+		health += adjust;
+	}
+	void Start()
+	{
+		health = slider.maxValue;
+	}
+	void Update()
+	{
+		slider.value = health;
+	}
+}
